19668bef548158c96ab45c3d36b22622c5e1442e
sebastian Tovar Chavez <sebastiantovarchavez304@gmail.com>
2023-01-06 14:05:27 -0500

    mi primer commit

---
+++ minecraft/src/App.jsx
@@ -0,0 +1,23 @@
+import {Canvas} from '@react-three/fiber'
+import {Sky} from '@react-three/drei'
+import {Physics} from '@react-three/cannon'
+import { Ground } from './components/Ground'
+import { FPV } from './components/FPV'
+import { Player } from './components/Player'
+function App () {
+  return (
+    
+      <Canvas>
+        <Sky sunPosition={[100,100,20]}/>
+        <ambientLight intensity={0.5}/>
+        <FPV/>
+        <Physics>
+        
+          <Ground/>
+        </Physics>
+      </Canvas>
+     
+  )
+}
+
+export default App
